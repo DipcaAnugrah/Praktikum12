31e08fef35a680b3606b2fcff693057ac7c44062
dipca0895 <@dipcaanugrah26.com>
2022-12-22 18:50:44 +0700

    String

--- latihan.py
+++ latihan.py
@@ -1,13 +1,7 @@
-# Praktikum12
-## String
-```
-Nama  : Dipca Anugrah
-NIM   : 312210666
-Kelas : TI.22.A.4
-```
-# Code dan Penjelasan Program
-* Latihan 1 
-``` python
+print("="*12, " STRING ", "="*12)
+print()
+print(f"{'latihan 1':12}")
+print('-'*12)
 txt = 'Hello World'
 print(f"Teks = {txt}")
 # Menghitung jumlah karakter
@@ -30,16 +24,10 @@ print(f"Teks menjadi huruf kecil = {e}")
 # Ganti karakter H dengan karakter J
 f = txt.replace("H","J")
 print(f"Menganti karakter H dengan J = {f}")
-```
-* Latihan 2 
-``` python
+print()
+print(f"{'latihan 2':12}")
+print('-'*12)
 umur = 24
 txt = 'Hello, nama saya john, dan umur saya adalah {0} tahun'
 print(txt.format(umur))
-```
-## Output Program
-* Latihan 1
-![png](gambar/latihan%201.png)
 
-* Latihan 2 
-![png](gambar/latihan%202.png)
